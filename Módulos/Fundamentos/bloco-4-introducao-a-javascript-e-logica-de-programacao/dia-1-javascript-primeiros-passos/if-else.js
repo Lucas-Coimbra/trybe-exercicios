b975f52fb7adc183910e50cf61b5c4b0c76265f8
lucascoimbra <lucas.gabriel_coimbra@hotmail.com>
2022-04-06 19:28:08 -0300

    Realizado o exercicio de fixação if/else

---
+++ Módulos/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/if-else.js
@@ -0,0 +1,11 @@
+const notaDoJao = 78;
+
+if (notaDoJao >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (notaDoJao < 80 && notaDoJao >= 60) {
+  console.log("Você está na nossa lista de espera");
+}
+else {
+   console.log("Você foi reprovada(o)");
+} 
