71d7ed9501ac4c9c59bb903f1c86d5b399d50790
lucascoimbra <lucas.gabriel_coimbra@hotmail.com>
2022-04-07 15:39:35 -0300

    Realizado exercicio de fixação switch/case

---
+++ Módulos/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/switch-case.js
@@ -0,0 +1,14 @@
+let pessoaCandidata = 'reprovada';
+switch (pessoaCandidata) {
+    case 'aprovada':
+        console.log("Você passou!!!");
+        break;
+    case 'lista':
+        console.log("Sossega aí, vai que ainda dá");
+        break;
+    case 'reprovada':
+        console.log("Mais sorte na próxima vez");
+        break;
+    default: 
+    console.log('não se aplica');
+}
