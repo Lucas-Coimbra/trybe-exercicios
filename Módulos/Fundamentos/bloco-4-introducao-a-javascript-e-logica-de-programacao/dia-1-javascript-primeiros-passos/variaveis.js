4fe2576bad58e6ffaf06ef902215c92846fb508a
lucascoimbra <lucas.gabriel_coimbra@hotmail.com>
2022-09-12 21:17:31 -0300

    Exercicios 4.1

--- Módulos/Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/variaveis.js
+++
@@ -1,8 +0,0 @@
-const myName = "Lucas";
-const birthCity = "Arcoverde";
-let birthYear = 2002;
-birthYear = 2030;
-//birthCity = "Caruaru"; (Constantes não podem ser alteradas!!!)
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
